e343edf2d9cb437ffb6633fc3f8b26d8ca2a5f49
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-19 16:20:05 +0200

    Add project files.

---
+++ CurrentDateAndTime/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        txtDateAndTime = New Label()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' txtDateAndTime
+        ' 
+        txtDateAndTime.AutoSize = True
+        txtDateAndTime.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        txtDateAndTime.Location = New Point(219, 102)
+        txtDateAndTime.Name = "txtDateAndTime"
+        txtDateAndTime.Size = New Size(271, 48)
+        txtDateAndTime.TabIndex = 0
+        txtDateAndTime.Text = "Date And Time"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 1000
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtDateAndTime)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtDateAndTime As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ CurrentDateAndTime/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        txtDateAndTime.Text = Date.Now.ToString("dd MMM yyyy HH:mm:ss")
+    End Sub
+End Class
